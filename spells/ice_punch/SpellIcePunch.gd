dc184f803abcf5772cd82ce1bbf61fef3753fe13
Tyler <47481160+Tyler-Henson@users.noreply.github.com>
2023-09-11 19:33:11 -0700

    finished ice fist spell and bullet

---
+++ spells/ice_punch/SpellIcePunch.gd
@@ -0,0 +1,13 @@
+class_name SpellIcePunch
+extends Spell
+
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("shoot") and _cooldown_timer.is_stopped():
+		shoot()
+
+
+func shoot() -> void:
+	_cooldown_timer.start()
+	.shoot()
