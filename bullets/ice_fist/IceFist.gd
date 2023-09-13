cf791a5df4127c1b6e1b6c73fc9a1f998880ce0b
Tyler <47481160+Tyler-Henson@users.noreply.github.com>
2023-09-12 22:04:50 -0700

    finished basic spells and bullets

--- bullets/ice_fist/IceFist.gd
+++ bullets/ice_fist/IceFist.gd
@@ -2,6 +2,10 @@ extends Bullet
 
 onready var _animation_player := $AnimationPlayer
 
+var max_acceleration = speed * 2
+
+export (float, 0.0, 1.0, 0.01) var acceleration_rate := 0.5
+
 func _ready() -> void:
 	_animation_player.connect("animation_finished", self, "_on_AnimationPlayer_animation_finished")
 	_animation_player.play("spawn")
@@ -16,3 +20,18 @@ func _destroy():
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "destroy":
 		queue_free()
+
+func _move(delta: float) -> void:
+	speed = lerp(speed, max_acceleration, acceleration_rate)
+	var distance := speed * delta
+	var motion := transform.x * speed * delta
+
+	position += motion
+
+	_travelled_distance += distance
+	if _travelled_distance > max_range:
+		_destroy()
+
+
+
+
