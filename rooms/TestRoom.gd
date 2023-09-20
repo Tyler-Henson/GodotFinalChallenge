213c973c6dcfe92fd1e58de3370d0fb4ab1e602a
Tyler <47481160+Tyler-Henson@users.noreply.github.com>
2023-09-19 23:08:14 -0700

    Added spawners for mobs, teleporters, and the character

--- rooms/TestRoom.gd
+++ rooms/TestRoom.gd
@@ -1,11 +1,14 @@
 extends YSort
 
-onready var spawner := $Mobs/Spawner
+onready var spawner := $Mobs/SpawnerMob
+
+onready var spawner_teleport := $Mobs/SpawnerTeleporter
 
 
 
 func _ready() -> void:
 	spawner.spawn()
+	#spawner_teleport.spawn()
 
 
 
